79229f76e772347c6fb6d70d695ff791dd7e50a7
Bansi Kasundra <kasundra@amazon.com>
2023-04-06 13:33:48 -0700

    [Snapshot Interop] Add Changes in Snapshot Status Flow for remote store interoperability.
    Signed-off-by: Bansi Kasundra <kasundra@amazon.com>

--- server/src/main/java/org/opensearch/repositories/FilterRepository.java
+++ server/src/main/java/org/opensearch/repositories/FilterRepository.java
@@ -220,8 +220,13 @@ public class FilterRepository implements Repository {
     }
 
     @Override
-    public IndexShardSnapshotStatus getShardSnapshotStatus(SnapshotId snapshotId, IndexId indexId, ShardId shardId) {
-        return in.getShardSnapshotStatus(snapshotId, indexId, shardId);
+    public IndexShardSnapshotStatus getShardSnapshotStatus(
+        SnapshotId snapshotId,
+        IndexId indexId,
+        ShardId shardId,
+        boolean isRemoteIndexShard
+    ) {
+        return in.getShardSnapshotStatus(snapshotId, indexId, shardId, isRemoteIndexShard);
     }
 
     @Override
